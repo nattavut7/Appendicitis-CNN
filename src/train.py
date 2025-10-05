91d1f8ae0e79466babd095983f2a4647e46d14bc
nattavut7 <nattavut.sr@ksu.ac.th>
2025-10-05 15:54:54 +0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,2 +1,34 @@
-# Training script placeholder
-print('Training script loaded.')
+
+import torch
+from torch import nn, optim
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+from model import UltraLightAppendicitisNet
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = UltraLightAppendicitisNet().to(device)
+
+transform = transforms.Compose([
+    transforms.Grayscale(),
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+])
+
+train_dataset = datasets.ImageFolder(root='./data/train', transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+for epoch in range(20):
+    model.train()
+    total_loss = 0
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.float().to(device).view(-1, 1)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    print(f"Epoch {epoch+1}, Loss: {total_loss:.4f}")
