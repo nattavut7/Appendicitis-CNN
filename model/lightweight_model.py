d175e234d80a3436498174869a631c08f30265b8
nattavut7 <nattavut.sr@ksu.ac.th>
2025-10-05 16:11:38 +0700

    Add files via upload

---
+++ model/lightweight_model.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+class LightweightCNN(nn.Module):
+    def __init__(self, num_classes=2):
+        super(LightweightCNN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(32 * 64 * 64, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
